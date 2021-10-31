e3e580c909335b28312ef30a7d597516166fb712
Olik Sanina <Rinpoche.Despot@yandex.ru>
2021-10-31 15:26:55 +0300

    1

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
 // const errors = require('celebrate');
 require('celebrate');
 const express = require('express');
+// const helmet = require('helmet');// Helmet helps you secure your Express apps by setting various HTTP headers
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');// не нужен
 const { errors } = require('celebrate');
 const router = require('express').Router(); // корневой роутер
+const { logger } = require('express-winston');
+// const rateLimit = require('express-rate-limit');
+
 const routes = require('./routes/users');
 const cardRoutes = require('./routes/cards');
 require('./middlewares/auth');
@@ -12,42 +16,80 @@ const NotFoundError = require('./errors/NotFoundError');
 
 const { createUser, login } = require('./controllers/users');
 
-const PORT = 3623;
+// require('dotenv').config();
+
+const { PORT = 3624 } = process.env;
+
 const app = express();
+// app.use(helmet());
+//  app.disable('x-powered-by'); // защитим от  использования этот заголовок (включенный по умолчанию)
+// для выявления приложений на базе Express и активации целенаправленных атак
+/*
+
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 60 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'limit each IP to 100 requests  Превышен лимит запросов с Вашего IP',
+});
+*/
+/*
+const apiLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 60 minutes
+  max: 30, // start blocking after 30 requests
+  message: 'start blocking after 30 requests Заблокировано по кол-ву запросов ',
+});
+*/
+
+//  apply to all requests
+// app.use(limiter);
+
+/* // only apply to requests that begin with /api/
+app.use('/api/', apiLimiter); */
+
 const url = 'mongodb://localhost:27017/mestodb';
 const auth = require('./middlewares/auth');
 const { loginValidate, userValidate } = require('./validator/validator');
-// const {logger} = require("express-winston");
-// const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 mongoose.connect(url, { useNewUrlParser: true }, { useNewUrlParser: true });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Логгер запросов до  роутов
-// app.use(requestLogger);
-// app.use(logger);
+//app.use(requestLogger);
+//app.use(logger);
+
+/*app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});*/
+
 app.post('/signin', loginValidate, login);
 app.post('/signup', userValidate, createUser);
 app.use(auth);// все роуты ниже этой строки будут защищены
 app.use(routes);
 app.use(cardRoutes);
 
-// errorLogger нужно подключить после обработчиков роутов и до обработчиков ошибок
-// app.use(errorLogger);
-
-app.use(errors()); // обработчик ошибок celebrate
-
 app.use(() => {
   throw new NotFoundError('Нет такой страницЫ');
 });
+
+// errorLogger нужно подключить после обработчиков роутов и до обработчиков ошибок
+app.use(errorLogger);
+app.use(errors()); // обработчик ошибок celebrate
+
+
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
   next();
 });
-
 app.listen(PORT, () => {
   console.log(`Express is Working in console ${PORT}`);
 });
 module.exports = { router };
+
+//  Введите ограничение скорости передачи данных например, express-limiter
+// обработчик csurf для защиты от подделки межсайтовых запросов (CSRF).
+// Используйте safe-regex, чтобы убедиться в невосприимчивости регулярных выражений к атакам
--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -1,23 +1,26 @@
-/*
 // импортируем нужные модули
-const winston = require('winston');
+const winston = require('winston');// библиотека для логирования
 const expressWinston = require('express-winston');
-const { request } = require('express');
+const path = require('path');
+
+const pathWithLogs = path.join(__dirname, '../logs');
+
+// winston.add(winston.transports.File, { filename: path.join(__dirname, 'error.log') });
 
 // запросы к серверу будем логировать.  Логгер запросов.
 const requestLogger = expressWinston.logger({
-  transports: [new winston.transports.File({ filename: 'request.log' }),
+  transports: [new winston.transports.File({ filename: path.join(pathWithLogs, 'request.log') }),
   ], // куда нужно писать лог.(логи можно писать в консоль или в сторонний сервис аналитики,
   // но мы ограничимся файлом)
+  level: 'info',
   format: winston.format.json(), // формат записи логов json
 });
 
 // ошибки сервера будем логировать. В лог
 // записывалась информация о ней — имя ошибки, сообщение и её стектрейс
 const errorLogger = expressWinston.errorLogger({
-  transports: [new winston.transports.File({
-    filename: 'error.log',
-  })],
+  transports: [new winston.transports.File({ filename: path.join(pathWithLogs, 'error.log') })],
+  level: 'info',
   format: winston.format.json(),
 });
 
@@ -25,4 +28,3 @@ module.exports = {
   requestLogger,
   errorLogger,
 };
-*/
